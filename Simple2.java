66513d468620374a97ef62c4157490381e297a46
PrasannaKV1 <prasannapooja215@gmail.com>
2024-10-18 22:17:48 +0530

    Sample2.java modified file

--- Simple2.java
+++ Simple2.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Simple2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String sone =sc.nextLine();
+        int CapCount=0;
+        int smallcount=0;
+        for(int i=0;i<sone.length();i++)
+        {
+            if(sone.charAt(i)>='A'&& sone.charAt(i)<='Z')
+            {
+                CapCount++;
+            }
+            else if(sone.charAt(i)>='a'&&sone.charAt(i)<='z')
+            {
+                smallcount++;
+            }
+        }
+
+        System.out.println("Number of capital letters are"+CapCount);
+        System.out.println("Number of small letter are"+smallcount);
+
+
+     sc.close();
+
+    }
     
 }
